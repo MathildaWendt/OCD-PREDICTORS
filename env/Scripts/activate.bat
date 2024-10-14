e73030a1d6ef2e167130833a7980ebc2c2743051
ShwetaGhaisas <163859671+ShwetaGhaisas@users.noreply.github.com>
2024-10-14 22:16:38 +0200

    support

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jaaha\Documents\Sidra Karolinska\3rd Semester\DSV - Project Management toold\OCD-PREDICTORS\env
+set VIRTUAL_ENV=C:\Users\shwet\Documents\GitHub\OCD-PREDICTORS\env
 
 if not defined PROMPT set PROMPT=$P$G
 
